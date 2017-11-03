acb99d6cc47e3ee0c09cd3d22f3722f3cb319951
adshaw <aditya.s.shaw@gmail.com>
2017-11-04 01:51:31 +0530

    Auto-generated commit

---
+++ assets/workExperience.ts
@@ -0,0 +1,167 @@
+export const timeLineObj = [
+    {
+    'year': '2016',
+    'headerTitle': 'JPMorgan Chase & Co.',
+    'workList': [
+      'Recruited as the only Angular2 Developer in the team and was responsibile of creating a responsive CRM application',
+      'Also designed and developed a Dashboard view of the application to give a visual representation to hundreds of data using highcharts and Ag grid',
+      'Participated in design discussions and overview and also was asked to create a blueprint for the application architecture and structure',
+      'Mastered Angular within a short span and was a mentor to developers who wanted to move to the new version of Angular'
+    ],
+    'skills': [
+      {
+        'name': 'Typescript',
+        'value': 30
+      },
+      {
+        'name': 'HTML/CSS',
+        'value': 45
+      },
+      {
+        'name': 'Angular',
+        'value': 40
+      },
+      {
+        'name': 'Java',
+        'value': 10
+      }
+    ]
+  },
+  {
+    'year': '2015',
+    'headerTitle': 'Capgemini - Hewlett-Packard',
+    'workList': [
+      'Worked as a UI developer to develop user interface and interact with the server side logic written in c#',
+      'Application worked across mobile devices sand modern desktop browsers using HTML5 and Bootstrap',
+      'Involved in designing and developing user interface using HTML, CSS, Jquery/Javascript',
+      'Also Implemented business logic by creating Controllers in Salesforce'
+    ],
+    'skills': [
+      {
+        'name': 'Javascript',
+        'value': 30
+      },
+      {
+        'name': 'HTML/CSS',
+        'value': 30
+      },
+      {
+        'name': 'Angular 1.x',
+        'value': 40
+      },
+      {
+        'name': 'Bootstrap',
+        'value': 30
+      }
+    ]
+  },
+  // {
+  //   'year': '2015',
+  //   'headerTitle': 'Capgemini - Hewlett-Packard',
+  //   'workList': [
+  //     'Using Salesforce, HP is providing a customer/partner support user interface where one can log all the questions related to HP products and services',
+  //     'Involved in designing and developing user interface using HTML, CSS, Jquery/Javascript',
+  //     'Also Implemented business logic by creating Controllers in Salesforce'
+  //   ],
+  //   'skills': [
+  //     {
+  //       'name': 'Javascript',
+  //       'value': 30
+  //     },
+  //     {
+  //       'name': 'HTML/CSS',
+  //       'value': 70
+  //     },
+  //     {
+  //       'name': 'BOOTSTRAP',
+  //       'value': 20
+  //     },
+  //     {
+  //       'name': 'SalesForce',
+  //       'value': 10
+  //     }
+  //   ]
+  // },
+  {
+    'year': '2014',
+    'headerTitle': 'TCS - Singapore Airlines',
+    'workList': [
+      'Involved in developing user interface using HTML, CSS, Jquery, Javascript, AngularJS',
+      'Implemented Server side business logic in Java/J2EE and Spring',
+      'Work closely with user interface developers in integrating web components with business logics'
+    ],
+    'skills': [
+      {
+        'name': 'Javascript',
+        'value': 30
+      },
+      {
+        'name': 'HTML/CSS',
+        'value': 45
+      },
+      {
+        'name': 'Angular 1.x',
+        'value': 30
+      },
+      {
+        'name': 'Spring',
+        'value': 20
+      }
+    ]
+  },
+  {
+    'year': '2011',
+    'headerTitle': 'TCS - Internal',
+    'workList': [
+      'Involved with Requirement gathering process and developing user interface using HTML, CSS, Jquery/Javascript',
+      'Implemented Server side business logic in Java/J2EE and Android',
+      'Worked closely with user interface developers in integrating web components with business logics',
+      'Worked with AR(Augmented Reality) and QR(Quick Response) for the Android module'
+    ],
+    'skills': [
+      {
+        'name': 'Javascript',
+        'value': 30
+      },
+      {
+        'name': 'HTML/CSS',
+        'value': 30
+      },
+      {
+        'name': 'Android',
+        'value': 20
+      },
+      {
+        'name': 'Java',
+        'value': 20
+      }
+    ]
+  },
+  // {
+  //   'year': '2011',
+  //   'headerTitle': 'TCS - Incubator Project',
+  //   'workList': [
+  //     'Elegantly involved in developing user interface using HTML, CSS, Jquery/Javascript',
+  //     'Implemented Server side business logic in Java/J2EE',
+  //     'Worked closely with user interface developers in integrating web components with business logics'
+  //   ],
+  //   'skills': [
+  //     {
+  //       'name': 'Javascript',
+  //       'value': 30
+  //     },
+  //     {
+  //       'name': 'HTML/CSS',
+  //       'value': 30
+  //     },
+  //     {
+  //       'name': 'BOOTSTRAP',
+  //       'value': 20
+  //     },
+  //     {
+  //       'name': 'Java',
+  //       'value': 20
+  //     }
+  //   ]
+  // }
+];
